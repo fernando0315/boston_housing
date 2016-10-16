b38ba63912b4c8d00cf92abdbe7ce7935988ed5e
Gerry Fernando Patia <Gerry Fernando Patia>
2016-10-15 20:57:03 -0700

    Update Answer Q2 & make main.py to work

--- main.py
+++ main.py
@@ -1,9 +1,49 @@
 import numpy as np
 import pandas as pd
-from sklearn.cross_validation import shufflesplit
+import visuals as vs
+#from sklearn.cross_validation
 
 data = pd.read_csv('housing.csv')
 prices = data['MEDV']
 features = data.drop('MEDV', axis = 1)
 
 print "Boston housing dataset has {} data points with {} variable each.".format(*data.shape)
+
+#Part 2
+# TODO: Minimum price of the data
+minimum_price = min(prices)
+
+# TODO: Maximum price of the data
+maximum_price = max(prices)
+
+# TODO: Mean price of the data
+mean_price = np.mean(prices)
+
+# TODO: Median price of the data
+median_price = np.median(prices)
+
+# TODO: Standard deviation of prices of the data
+std_price = np.std(prices)
+
+# Show the calculated statistics
+print "Statistics for Boston housing dataset:\n"
+print "Minimum price: ${:,.2f}".format(minimum_price)
+print "Maximum price: ${:,.2f}".format(maximum_price)
+print "Mean price: ${:,.2f}".format(mean_price)
+print "Median price ${:,.2f}".format(median_price)
+print "Standard deviation of prices: ${:,.2f}".format(std_price)
+
+#Checking Data
+import matplotlib.pyplot as plt
+for col in features.columns:
+
+    fig, ax = plt.subplots()
+    fit = np.polyfit(features [col], prices, deg=1) # We use a linear fit to compute the trendline
+    ax.scatter(features [col],  prices)
+    plt.plot(features [col], prices, 'o', color='black')
+    ax.plot(features[col], fit[0] * features[col] + fit[1], color='blue', linewidth=3) # This plots a trendline with the regression parameters computed earlier. We should plot this after the dots or it will be covered by the dots themselves
+    plt.title('PRICES vs  '+ str(col)) # title here
+    plt.xlabel(col) # label here
+    plt.ylabel('PRICES') # label here
+
+plt.show()
